9e09bc5c2145506b2ec2b5596387521beba89c72
roksentiuk <2>
2025-04-06 16:58:25 +0200

    ckad_deprecated_api2

---
+++ ckad_deprecated_api_task2/check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+INGRESS_API_VERSION=$(kubectl get ingress myapp-ingress -o jsonpath='{.apiVersion}')
+if [ "$INGRESS_API_VERSION" == "networking.k8s.io/v1" ]; then
+  echo "Success: Ingress is using networking.k8s.io/v1"
+  exit 0
+else
+  echo "Error: Ingress is not using networking.k8s.io/v1, current value: $INGRESS_API_VERSION"
+  exit 1
+fi
